9534efa09bb9e2aa4b703639e49dc903944218a3
Brian Hardy <hardyr.93@gmail.com>
2018-02-20 10:25:01 -0300

    Simulation log folder changed & build

--- build/ludorum-towerdefense.js
+++ build/ludorum-towerdefense.js
@@ -2078,49 +2078,42 @@ var scenarios = exports.scenarios = {
 		mapWidth: 8,
 		mapHeight: 8,
 		creepPath: [
-			[1,0], [1,1], [1,2], [1,3], [1,4], [2,4], [3,4], [3,5], [3,6], [4,6], [5,6], [5,5],
-			[5,4], [5,3], [5,2], [5,1], [5,0]
+			[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [2, 4], [3, 4], [3, 5], [3, 6], [4, 6], [5, 6], [5, 5],
+			[5, 4], [5, 3], [5, 2], [5, 1], [5, 0]
 		],
 		towerPlaces: [
-			[2,3], [4,5], [4,3], [2,1], [2,5]
+			[2, 3], [4, 5], [4, 3], [2, 1], [2, 5]
 		],
 		waves: [
 			[10, [[1, "Mario"], [400, "Mario"], [1000, "Rope"], [1200, "Rope"]]],
 
 			[12, [[1, "Mario"], [350, "Rope"],
-				  [850, "Mario"], [1100, "Mario"],
-				  [1800, "Rope"], [2500, "Rope"]]],
+			[850, "Mario"], [1100, "Mario"],
+			[1800, "Rope"], [2500, "Rope"]]],
 
 			[16, [[1, "Mario"], [350, "Rope"], [500, "FireWizzrobe"],
-				  [1000, "Rope"], [1500, "FireWizzrobe"]]],
+			[1000, "Rope"], [1500, "FireWizzrobe"]]],
 
 			[20, [[1, "Speedy"], [200, "FireWizzrobe"], [500, "Rope"],
-				  [600, "FireWizzrobe"], [900, "FireWizzrobe"],
-				  [1300, "FireWizzrobe"], [2800, "DarkNut"]]],
+			[600, "FireWizzrobe"], [900, "FireWizzrobe"],
+			[1300, "FireWizzrobe"], [2800, "DarkNut"]]],
 
 			[22, [[1, "FireWizzrobe"], [600, "Rope"],
-				  [1500, "FireWizzrobe"], [2000, "Rope"],
-				  [2800, "FireWizzrobe"], [3700, "DarkNut"], [4500, "DarkNut"],
-				  [6000, "FireWizzrobe"]]],
+			[1500, "FireWizzrobe"], [2000, "Rope"],
+			[2800, "FireWizzrobe"], [3700, "DarkNut"], [4500, "DarkNut"],
+			[6000, "FireWizzrobe"]]],
 
-		  	[25, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"],
-				  [1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
-				  [4000, "FireWizzrobe"]]],
+			[25, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"],
+			[1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
+			[4000, "FireWizzrobe"]]],
 
 			[25, [[1, "DarkNut"], [600, "FireWizzrobe"], [1000, "DarkNut"],
-				  [2200, "Rope"], [2500, "Rope"], [2600, "Rope"],
-				  [4000, "FireWizzrobe"], [4900, "FireWizzrobe"], [6000, "Armos"]]],
+			[2200, "Rope"], [2500, "Rope"], [2600, "Rope"],
+			[4000, "FireWizzrobe"], [4900, "FireWizzrobe"], [6000, "Armos"]]],
 
 			[30, [[1, "DarkNut"], [1000, "DarkNut"],
-				  [2200, "FireWizzrobe"], [2800, "FireWizzrobe"],
-				  [4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"]]],
-
-			[50, [[1, "Armos"], [1000, "Rope"], [1400, "Rope"],
-				  [3000, "Armos"], [4800, "FireWizzrobe"], [5100, "FireWizzrobe"],
-				  [6000, "Armos"], [7100, "DarkNut"], [7800, "DarkNut"],
-				  [8500, "Armos"], [10000, "Armos"]]],
-
-			[25, [[1, "Armos"],[1000, "Armos"],[2000, "Armos"],[3000, "Armos"],[4000, "Armos"],[5000, "Armos"]]]
+			[2200, "FireWizzrobe"], [2800, "FireWizzrobe"],
+			[4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"]]]
 		],
 		startMoney: 25,
 		startHP: 10
@@ -2133,50 +2126,43 @@ var scenarios = exports.scenarios = {
 		mapWidth: 8,
 		mapHeight: 8,
 		creepPath: [
-			[0,4], [1,4], [2,4], [2,5], [2,6], [3,6], [4,6], [4,5], [4,4], [4,3], [4,2], [5,2],
-			[6,2], [7,2]
+			[0, 4], [1, 4], [2, 4], [2, 5], [2, 6], [3, 6], [4, 6], [4, 5], [4, 4], [4, 3], [4, 2], [5, 2],
+			[6, 2], [7, 2]
 		],
 		towerPlaces: [
-			[3,4], [1,5], [5,3]
+			[3, 4], [1, 5], [5, 3]
 		],
 		waves: [
 
 			[10, [[1, "Mario"], [400, "Mario"], [1000, "Rope"], [1200, "Rope"]]],
 
 			[12, [[1, "Mario"], [350, "Rope"],
-				  [850, "Mario"], [1100, "Mario"],
-				  [1800, "Rope"], [2500, "Rope"]]],
+			[850, "Mario"], [1100, "Mario"],
+			[1800, "Rope"], [2500, "Rope"]]],
 
 			[16, [[1, "Mario"], [350, "Rope"], [500, "FireWizzrobe"],
-				  [800, "FireWizzrobe"], [1000, "Rope"], [1500, "FireWizzrobe"]]],
+			[800, "FireWizzrobe"], [1000, "Rope"], [1500, "FireWizzrobe"]]],
 
 			[20, [[1, "Speedy"], [200, "FireWizzrobe"], [500, "Rope"], [550, "Rope"],
-				  [600, "FireWizzrobe"], [680, "Rope"], [900, "FireWizzrobe"],
-				  [1300, "FireWizzrobe"], [2800, "DarkNut"]]],
+			[600, "FireWizzrobe"], [680, "Rope"], [900, "FireWizzrobe"],
+			[1300, "FireWizzrobe"], [2800, "DarkNut"]]],
 
 			[22, [[1, "FireWizzrobe"], [500, "Rope"], [550, "Rope"],
-				  [1000, "FireWizzrobe"], [1200, "Rope"], [1350, "Rope"],
-				  [1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
-				  [4000, "FireWizzrobe"], [4200, "FireWizzrobe"]]],
+			[1000, "FireWizzrobe"], [1200, "Rope"], [1350, "Rope"],
+			[1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
+			[4000, "FireWizzrobe"], [4200, "FireWizzrobe"]]],
 
-		  	[25, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"], [1200, "DarkNut"],
-				  [1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
-				  [4000, "FireWizzrobe"], [4200, "DarkNut"]]],
+			[25, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"], [1200, "DarkNut"],
+			[1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
+			[4000, "FireWizzrobe"], [4200, "DarkNut"]]],
 
 			[20, [[1, "DarkNut"], [250, "FireWizzrobe"], [600, "FireWizzrobe"], [1000, "DarkNut"],
-				  [2200, "Rope"], [2300, "Rope"], [2400, "Rope"], [2500, "Rope"], [2600, "Rope"],
-				  [4000, "FireWizzrobe"], [4200, "DarkNut"], [4400, "FireWizzrobe"], [6000, "Armos"]]],
+			[2200, "Rope"], [2300, "Rope"], [2400, "Rope"], [2500, "Rope"], [2600, "Rope"],
+			[4000, "FireWizzrobe"], [4200, "DarkNut"], [4400, "FireWizzrobe"], [6000, "Armos"]]],
 
 			[25, [[1, "DarkNut"], [250, "DarkNut"], [600, "DarkNut"], [1000, "DarkNut"],
-				  [2200, "FireWizzrobe"], [2300, "FireWizzrobe"], [2400, "Rope"], [2500, "Rope"], [2600, "FireWizzrobe"],
-				  [4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"], [8000, "Armos"]]],
-
-			[30, [[1, "Armos"], [1000, "Rope"], [1300, "Rope"], [1400, "Rope"],
-				  [3000, "Armos"], [3100, "FireWizzrobe"], [3300, "FireWizzrobe"], [3400, "FireWizzrobe"],
-				  [5000, "Armos"], [5100, "DarkNut"], [5300, "DarkNut"], [5400, "DarkNut"],
-				  [6500, "Armos"], [7000, "Armos"]]],
-
-			[25, [[1, "Armos"],[1000, "Armos"],[2000, "Armos"],[3000, "Armos"],[4000, "Armos"],[5000, "Armos"]]]
+			[2200, "FireWizzrobe"], [2300, "FireWizzrobe"], [2400, "Rope"], [2500, "Rope"], [2600, "FireWizzrobe"],
+			[4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"], [8000, "Armos"]]]
 		],
 		startMoney: 15,
 		startHP: 10
@@ -2190,52 +2176,45 @@ var scenarios = exports.scenarios = {
 		mapHeight: 8,
 		creepPath: [
 			//[1,0], [1,1], [1,2], [2,2], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [7,5]
-			[0,4], [1,4], [2,4], [3,4], [4,4], [4,3], [4,2], [5,2], [6,2], [7,2]
+			[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [4, 3], [4, 2], [5, 2], [6, 2], [7, 2]
 		],
 		towerPlaces: [
 			//[2,3], [4,4], [2,1], [5,6]
-			[2,3], [4,5], [5,3], [2,5]
+			[2, 3], [4, 5], [5, 3], [2, 5]
 		],
 		waves: [
 			[8, [[1, "Mario"], [700, "Mario"], [1800, "Rope"], [2800, "Rope"], [4000, "DarkNut"]]],
 
 			[8, [[1, "Mario"], [350, "Mario"], [700, "Mario"],
-				  [1800, "Rope"], [2300, "Rope"], [2800, "Rope"],
-				  [3800, "FireWizzrobe"], [5000, "DarkNut"]]],
+			[1800, "Rope"], [2300, "Rope"], [2800, "Rope"],
+			[3800, "FireWizzrobe"], [5000, "DarkNut"]]],
 
-			[9, [[1, "Mario"], [350, "Rope"],  [400, "Mario"], [600, "Rope"],
-				  [800, "Rope"], [1000, "Rope"], [1500, "FireWizzrobe"]]],
+			[9, [[1, "Mario"], [350, "Rope"], [400, "Mario"], [600, "Rope"],
+			[800, "Rope"], [1000, "Rope"], [1500, "FireWizzrobe"]]],
 
 			[12, [[1, "FireWizzrobe"], [450, "Rope"],
-				  [600, "FireWizzrobe"], [900, "FireWizzrobe"], [1200, "FireWizzrobe"]]],
+			[600, "FireWizzrobe"], [900, "FireWizzrobe"], [1200, "FireWizzrobe"]]],
 
 			[15, [[1, "FireWizzrobe"], [500, "Rope"], [550, "Rope"],
-				  [600, "Rope"], [650, "Rope"], [700, "Rope"],
-				  [1600, "FireWizzrobe"], [4000, "FireWizzrobe"], [4200, "FireWizzrobe"]]],
+			[600, "Rope"], [650, "Rope"], [700, "Rope"],
+			[1600, "FireWizzrobe"], [4000, "FireWizzrobe"], [4200, "FireWizzrobe"]]],
 
-		  	[20, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"], [1200, "DarkNut"],
-				  [1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
-				  [4000, "FireWizzrobe"], [4200, "DarkNut"]]],
+			[20, [[1, "DarkNut"], [500, "FireWizzrobe"], [1000, "FireWizzrobe"], [1200, "DarkNut"],
+			[1600, "FireWizzrobe"], [2500, "DarkNut"], [3000, "DarkNut"],
+			[4000, "FireWizzrobe"], [4200, "DarkNut"]]],
 
 			[20, [[1, "DarkNut"], [250, "FireWizzrobe"], [600, "FireWizzrobe"], [1000, "DarkNut"],
-				  [2200, "Rope"], [2300, "Rope"], [2400, "Rope"], [2500, "Rope"], [2600, "Rope"],
-				  [4000, "FireWizzrobe"], [4200, "DarkNut"], [4400, "FireWizzrobe"], [6000, "Armos"]]],
+			[2200, "Rope"], [2300, "Rope"], [2400, "Rope"], [2500, "Rope"], [2600, "Rope"],
+			[4000, "FireWizzrobe"], [4200, "DarkNut"], [4400, "FireWizzrobe"], [6000, "Armos"]]],
 
 			[25, [[1, "DarkNut"], [250, "DarkNut"], [600, "DarkNut"], [1000, "DarkNut"],
-				  [2200, "FireWizzrobe"], [2300, "FireWizzrobe"], [2400, "Rope"], [2500, "Rope"], [2600, "FireWizzrobe"],
-				  [4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"], [8000, "Armos"]]],
-
-			[30, [[1, "Armos"], [1000, "Rope"], [1300, "Rope"], [1400, "Rope"],
-				  [3000, "Armos"], [3100, "FireWizzrobe"], [3300, "FireWizzrobe"], [3400, "FireWizzrobe"],
-				  [5000, "Armos"], [5100, "DarkNut"], [5300, "DarkNut"], [5400, "DarkNut"],
-				  [6500, "Armos"], [7000, "Armos"]]],
-
-			[25, [[1, "Armos"],[1000, "Armos"],[2000, "Armos"],[3000, "Armos"],[4000, "Armos"],[5000, "Armos"]]]
+			[2200, "FireWizzrobe"], [2300, "FireWizzrobe"], [2400, "Rope"], [2500, "Rope"], [2600, "FireWizzrobe"],
+			[4000, "Armos"], [5000, "DarkNut"], [5400, "FireWizzrobe"], [6000, "FireWizzrobe"], [8000, "Armos"]]]
 		],
 		startMoney: 14,
 		startHP: 10
 	}) // Test03 y 04
-	
+
 }; // scenarios
 
 /** See __prologue__.js
--- tests/simulation.js
+++ tests/simulation.js
@@ -19,7 +19,7 @@ var path = require('path'),
 			{ module: ludorum_towerdefense },
 			{ module: inveniemus }
 		],
-		logFile: base.Text.formatDate(null, '"./tests/logs/simulation-"yyyymmdd-hhnnss".txt"'),
+		logFile: base.Text.formatDate(null, '"./logs/simulation-"yyyymmdd-hhnnss".txt"'),
 		maxDelay: 10000,
 		maxRetries: 1000
 	});
@@ -35,8 +35,8 @@ var jobFunction = function (ludorum_towerdefense, scenario, size, steps) {
 			'hp@0': g0.hp
 		};
 	return ai.play(g0, function (gameFrom, move, gameTo) {
-		r['money@'+ gameTo.level] = gameTo.money;
-		r['hp@'+ gameTo.level] = gameTo.hp;
+		r['money@' + gameTo.level] = gameTo.money;
+		r['hp@' + gameTo.level] = gameTo.hp;
 	}).then(function (gf) {
 		r.reached = gf.level;
 		r.result = gf.result();
@@ -55,33 +55,33 @@ var SCENARIOS = ['Test01', 'Test02', 'Test03'],
 base.Future.all(
 	base.Iterable.range(MATCH_COUNT).product(SCENARIOS, AI_SIZES).mapApply(function (i, scenario, aiSize) {
 		return server.schedule({
-			info: scenario +' with AI'+ aiSize +' #'+ i,
+			info: scenario + ' with AI' + aiSize + ' #' + i,
 			fun: jobFunction,
 			imports: ['ludorum-towerdefense'],
 			args: [scenario, aiSize, AI_STEPS]
 		}).then(function (data) {
 			var reached = data.reached;
 			STATS.add({ key: 'reached', scenario: data.scenario }, reached);
-			STATS.add({ key: 'reached'+ reached, scenario: data.scenario });
+			STATS.add({ key: 'reached' + reached, scenario: data.scenario });
 			STATS.add({ key: 'result', scenario: data.scenario }, data.result);
 			if (reached < 11) {
-				STATS.add({ key: 'defeats', scenario: data.scenario, level: reached}, data['money@'+ reached]);
+				STATS.add({ key: 'defeats', scenario: data.scenario, level: reached }, data['money@' + reached]);
 			} else {
-				STATS.add({ key: 'victories', scenario: data.scenario});
+				STATS.add({ key: 'victories', scenario: data.scenario });
 			}
 			for (var i = 0; i <= reached; i++) {
-				STATS.add({ key: 'money', scenario: data.scenario, level: i }, data['money@'+ i]);
-				STATS.add({ key: 'hp', scenario: data.scenario, level: i }, data['hp@'+ i]);
+				STATS.add({ key: 'money', scenario: data.scenario, level: i }, data['money@' + i]);
+				STATS.add({ key: 'hp', scenario: data.scenario, level: i }, data['hp@' + i]);
 			}
-			server.logger.info(data.scenario +','+ aiSize +','+ reached +','+ data['hp@'+ reached]);
+			server.logger.info(data.scenario + ',' + aiSize + ',' + reached + ',' + data['hp@' + reached]);
 		});
 	})
 ).then(function () {
-	server.logger.info("Statistics:\n"+ STATS);
+	server.logger.info("Statistics:\n" + STATS);
 	server.logger.info("Finished. Stopping server.");
 	//setTimeout(process.exit, 10);
 }, function (error) {
-	server.logger.error(error +'');
+	server.logger.error(error + '');
 	//setTimeout(process.exit, 10);
 });
 // fin
